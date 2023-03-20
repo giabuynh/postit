a8a53a190760042e331dba95abe51110dc9c0013
giabuynh <giabinh0899@gmail.com>
2023-03-20 16:32:07 +0700

    Logout

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -16,7 +16,7 @@ export default async function Nav() {
       </Link>
       <ul className='flex items-center gap-6'>
         {!session?.user && <Login />}
-        {session?.user && <h1>{session.user?.name}</h1>}
+        {session?.user && <Logged image={session.user?.image || ''} />}
       </ul>
     </nav>
   )
