06a4af0eb8b50dec7a572e03940e9f1f64b884be
giabuynh <giabinh0899@gmail.com>
2023-03-21 09:11:21 +0700

    Set up react-query

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Roboto } from 'next/font/google'
 
 import Nav from './auth/Nav'
+import QueryWrapper from './components/QueryWrapper'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -18,9 +19,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
-        <Nav />
-        {/* children load from page.tsx */}
-        {children}
+        <QueryWrapper>
+          <Nav />
+          {/* children load from page.tsx */}
+          {children}
+        </QueryWrapper>
       </body>
     </html>
   )
