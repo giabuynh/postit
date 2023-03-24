4c9e8a193137b18e5301b192a12c1130473e11fe
giabuynh <giabinh0899@gmail.com>
2023-03-24 11:52:34 +0700

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // comfortaa: ['Comfortaa', 'cursive'],
+        // inter: ['Inter', 'sans-serif'],
+        avenir: 'avenir'
+      }
+    },
   },
   plugins: [],
 }
